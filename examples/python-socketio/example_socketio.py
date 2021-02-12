341a3d8e5062cdd8f15edf534c88a7c6e33c55a3
Tyler Bryant <tyler@nstrumenta.com>
2021-02-11 17:08:00 -0800

    adds python socketio example

---
+++ examples/python-socketio/example_socketio.py
@@ -0,0 +1,22 @@
+import socketio
+
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print('connection established')
+    for i in range(1, 100):
+        sio.emit('sensor', {'foo': i})
+
+@sio.event
+def my_message(data):
+    print('message received with ', data)
+    sio.emit('my response', {'response': 'my response'})
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+sio.connect('http://localhost:8080')
+
+
